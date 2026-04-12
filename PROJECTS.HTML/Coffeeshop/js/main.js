9ed25d8a77f508d738c2ebb4225904c2c4c8dc64
MyatKaungKyaw242 <myatkaungmyat508@gmail.com>
2026-04-12 16:07:20 +0630

    first commit

---
+++ PROJECTS.HTML/Coffeeshop/js/main.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector("#menu");
+const navLinks = document.querySelector(".nav-links");
+
+menu.addEventListener("click",()=>{
+ navLinks.classList.toggle("show")
+
+ 
+ if(navLinks.classList.contains("show")){
+    menu.src = "./coffee_images/close-icon.svg"
+ }else{
+    menu.src = "./coffee_images/three-horizontal-lines-icon.svg"
+ }
+})
